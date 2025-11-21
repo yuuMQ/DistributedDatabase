24946aa0fd3a5f31569b1f03ed2e5c18bbfe4f1c
quan.pm_Yuu <minhquan7ahaha@gmail.com>
2025-11-21 11:42:13 +0700

    Add files via upload

---
+++ Bai giai cau 4 bai thuc hanh 4.sql
@@ -0,0 +1,75 @@
+﻿
+--Bài giải câu 4, bài thực hành 4
+
+-- tạo CSDL để demo
+use Master
+go
+CREATE DATABASE CSDL_2
+go
+
+-- chuyển dbowwner cho login khác
+use CSDL_2
+GO
+sp_changedbowner '513\Admin'
+
+-- chuyển dbowwner cho login khác
+use CSDL_2
+GO
+sp_changedbowner 'sa'
+
+-- xem thông tin 1 CSDL
+USE Master
+GO
+EXEC sp_helpdb CSDL_2
+go
+-- xem thông tin 1 CSDL
+USE Master
+GO
+EXEC sp_helpdb AdventureWorks
+
+-- xem thông tin tất cả CSDL
+USE Master
+GO
+EXEC sp_helpdb
+
+--Tạo mới Schema
+USE CSDL_2
+GO
+CREATE SCHEMA TenSchema
+go
+
+--Tạo bảng mới trong schema
+USE CSDL_2
+GO
+CREATE TABLE TenSchema.BangNhanVien
+(MaNV int IDENTITY(1,1) Primary key
+,HoNV varchar(75) NOT NULL
+,TenNV varchar(75) NOT NULL)
+
+--xóa schema
+USE CSDL_2
+GO
+DROP SCHEMA TenSchema
+
+--đổi 1 bảng sang schema khác
+USE CSDL_2
+GO
+ALTER SCHEMA dbo TRANSFER TenSchema.BangNhanVien
+
+--xóa bảng
+DROP TABLE dbo.BangNhanVien
+go
+
+-- xóa CSDL_2, có ngắt kết nối trước khi xóa
+use [master];
+go
+EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'CSDL_2'
+GO
+use [master];
+go
+ALTER DATABASE CSDL_2 SET  SINGLE_USER WITH ROLLBACK IMMEDIATE
+GO
+USE [master]
+GO
+DROP DATABASE CSDL_2
+GO
---
+++ File sql rename servername PMC_PM05.sql
@@ -0,0 +1,21 @@
+﻿
+-- Lớp QTHCSDL - Thầy Khải
+--File sql để rename servername từ 'D16' thành 'TênMáyTính'
+
+--Xem tên servername trước khi đổi tên
+
+use master
+go
+Select name as TênServerNameCũ from sys.servers
+go
+--Xóa tên servername cũ 'D16'
+sp_dropserver 'D16'
+go
+-- Thêm lại tên servername mới đúng với tên máy tính
+sp_addserver 'C41', 'local'
+go
+--Xem tên servername sau khi đổi tên
+select name as TênServerNameMới from sys.servers
+go
+Select N'Hãy restart SQL Server là đổi tên servername xong' AS ThôngBáo
+go
