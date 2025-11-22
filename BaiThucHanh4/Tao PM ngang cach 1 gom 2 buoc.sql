5fd676f70c35272690c66d575816b40430969405
quan.pm_Yuu <minhquan7ahaha@gmail.com>
2025-11-22 08:13:50 +0700

    Add files via upload

---
+++ BaiThucHanh4/Tao PM ngang cach 1 gom 2 buoc.sql
@@ -0,0 +1,50 @@
+﻿CREATE DATABASE Northwind1
+go
+
+---------Tao PM ngang cach 1 (gom 2 buoc: Tao thiet ke PM va lay DL cho PM)
+--Tao thiet ke PM KH1
+CREATE TABLE Northwind1.[dbo].[KH1]
+(
+	[CustomerID] [nchar](5) PRIMARY KEY,
+	[CompanyName] [nvarchar](40) NOT NULL,
+	[ContactName] [nvarchar](30) NULL,
+	[ContactTitle] [nvarchar](30) NULL,
+	[Address] [nvarchar](60) NULL,
+	[City] [nvarchar](15) NULL,
+	[Region] [nvarchar](15) NULL,
+	[PostalCode] [nvarchar](10) NULL,
+	[Country] [nvarchar](15) NULL,
+	[Phone] [nvarchar](24) NULL,
+	[Fax] [nvarchar](24) NULL
+	)
+	
+	--Lay DL cho KH1:
+	INSERT INTO Northwind1.dbo.KH1
+		SELECT * FROM Northwind.dbo.Customers
+		WHERE Country=N'USA' OR Country=N'UK'
+	
+	--Test KH1:
+		SELECT * FROM Northwind1.dbo.KH1	
+	
+	
+	--Tao thiet ke cho PM KH2:
+	CREATE TABLE Northwind1.[dbo].[KH2]
+(
+	[CustomerID] [nchar](5) PRIMARY KEY,
+	[CompanyName] [nvarchar](40) NOT NULL,
+	[ContactName] [nvarchar](30) NULL,
+	[ContactTitle] [nvarchar](30) NULL,
+	[Address] [nvarchar](60) NULL,
+	[City] [nvarchar](15) NULL,
+	[Region] [nvarchar](15) NULL,
+	[PostalCode] [nvarchar](10) NULL,
+	[Country] [nvarchar](15) NULL,
+	[Phone] [nvarchar](24) NULL,
+	[Fax] [nvarchar](24) NULL
+	)
+	--Lay DL cho KH2:
+	INSERT INTO Northwind1.dbo.KH2
+		SELECT * FROM Northwind.dbo.Customers
+		WHERE Country<>N'USA' AND Country<>N'UK'
+	--Test KH2:
+		SELECT * FROM Northwind1.dbo.KH2
---
+++ BaiThucHanh4/Tao PM ngang cach 2 chi gom 1 buoc.sql
@@ -0,0 +1,25 @@
+﻿CREATE DATABASE Northwind1
+go
+
+---------Tao PM ngang cach 2 (chi gom 1 buoc: Vua tao thiet ke PM vua lay DL cho PM)
+
+--Phai xoa 2 PM KH1, KH2 neu da co
+------------------------------------------------------
+
+--Tao KH1:
+SELECT * INTO Northwind1.dbo.KH1
+	FROM Northwind.dbo.Customers
+	WHERE Country=N'USA' OR Country=N'UK'
+
+--Test KH1:
+SELECT * FROM Northwind1.dbo.KH1
+------------------------------------------------------
+
+--Tao KH2:
+SELECT * INTO Northwind1.dbo.KH2
+	FROM Northwind.dbo.Customers
+	WHERE Country=N'USA' OR Country=N'UK'
+
+--Test KH2:
+SELECT * FROM Northwind1.dbo.KH2
+	
