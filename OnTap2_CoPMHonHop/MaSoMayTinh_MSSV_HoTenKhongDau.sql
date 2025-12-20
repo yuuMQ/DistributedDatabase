28852d5beff475cce2e3459808358797a98638ab
quan.pm_Yuu <minhquan7ahaha@gmail.com>
2025-12-20 09:48:33 +0700

    Add files via upload

---
+++ OnTap2_CoPMHonHop/MaSoMayTinh_MSSV_HoTenKhongDau.sql
@@ -0,0 +1,87 @@
+﻿----------------------ĐỀ THI MÁY CHẲN-----------
+MÔN: CSDLPT
+MSSV:
+HỌ TÊN SV:
+
+-----------------------------------------------------------------------
+
+PHẦN 1: Phân mảnh ngang chính và phân mảnh ngang dẫn xuất (4 điểm)
+
+Câu 1: (2 điểm) Hãy viết 2 Stored procedure tên: 
+(SV TỰ ĐẶT TÊN CHO CÁC PHÂN MẢNH NGANG)
+
+-- Tạo PROC TaoPM_Ngang_PhongBan:
+
+
+-- Test PROC TaoPM_Ngang_PhongBan:
+
+
+-- Tạo PROC TaoPM_Ngang_NhanVien:
+
+
+-- Test PROC TaoPM_Ngang_NhanVien:
+
+
+
+Câu 2: (2 điểm) Tạo stored procedure tên SuaPB_Ngang:
+
+-- Tạo PROC SuaPB_Ngang:
+
+
+-- Test PROC SuaPB_Ngang:
+
+
+------------------------------------------------------------------
+
+PHẦN 2: Phân mảnh dọc (4 điểm)
+
+Câu 3: (1 điểm) Hãy viết Stored procedure tên: TaoPM_Doc_PhongBan:
+PhongBan_Doc1(MaPB, TenPB)
+PhongBan_Doc2(MaPB, ChiNhanh)
+
+-- Tạo PROC TaoPM_Doc_PhongBan:
+
+
+-- Test PROC TaoPM_Doc_PhongBan:
+
+
+-Câu 4: (1 điểm) Hãy viết Stored procedure tên: XemPB_Doc:
+
+-- Tạo PROC XemPB_Doc:
+
+
+-- Test PROC XemPB_Doc:
+
+
+Câu 5: (2 điểm) Tạo stored procedure tên SuaPB_Doc:
+
+-- Tạo PROC SuaPB_Doc:
+
+
+-- Test PROC SuaPB_Doc:
+
+
+---------------------------------------------------
+
+
+PHẦN 3: Phân mảnh HỖN HỢP (2 điểm)
+
+Câu 6: (1 điểm) Tạo View tên DanhSachTatCaPB_HH:
+
+-- Tạo View DanhSachTatCaPB_HH:
+
+
+-- Test View DanhSachTatCaPB_HH:
+
+
+Câu 7: (1 điểm) Tạo stored procedure tên SuaPB_HH:
+
+-- Tạo PROC SuaPB_HH:
+
+
+-- Test PROC SuaPB_HH:
+
+
+--ĐỂ CÓ ĐIỂM, CÁC EM NHỚ LƯU FILE SQL NÀY VÀO Ổ S:\
+------------------------HẾT-----------------------------------------
+
---
+++ OnTap3_CoPMHonHop/MaSoMayTinh_MSSV_HoTenKhongDau_DeMayChan.sql
@@ -0,0 +1,87 @@
+﻿----------------------ĐỀ THI MÁY CHẲN-----------
+MÔN: CSDL NÂNG CAO, NGÀY THI: 05/05/2019
+MSSV:
+HỌ TÊN SV:
+
+-----------------------------------------------------------------------
+
+PHẦN 1: Phân mảnh ngang chính và phân mảnh ngang dẫn xuất (4 điểm)
+
+Câu 1: (2 điểm) Hãy viết 2 Stored procedure tên: 
+(SV TỰ ĐẶT TÊN CHO CÁC PHÂN MẢNH NGANG)
+
+-- Tạo PROC TaoPM_Ngang_PhongBan:
+
+
+-- Test PROC TaoPM_Ngang_PhongBan:
+
+
+-- Tạo PROC TaoPM_Ngang_NhanVien:
+
+
+-- Test PROC TaoPM_Ngang_NhanVien:
+
+
+
+Câu 2: (2 điểm) Tạo stored procedure tên SuaPB_Ngang:
+
+-- Tạo PROC SuaPB_Ngang:
+
+
+-- Test PROC SuaPB_Ngang:
+
+
+------------------------------------------------------------------
+
+PHẦN 2: Phân mảnh dọc (4 điểm)
+
+Câu 3: (1 điểm) Hãy viết Stored procedure tên: TaoPM_Doc_PhongBan:
+PhongBan_Doc1(MaPB, TenPB)
+PhongBan_Doc2(MaPB, ChiNhanh)
+
+-- Tạo PROC TaoPM_Doc_PhongBan:
+
+
+-- Test PROC TaoPM_Doc_PhongBan:
+
+
+-Câu 4: (1 điểm) Hãy viết Stored procedure tên: XemPB_Doc:
+
+-- Tạo PROC XemPB_Doc:
+
+
+-- Test PROC XemPB_Doc:
+
+
+Câu 5: (2 điểm) Tạo stored procedure tên ThemPB_Doc:
+
+-- Tạo PROC ThemPB_Doc:
+
+
+-- Test PROC ThemPB_Doc:
+
+
+---------------------------------------------------
+
+
+PHẦN 3: Phân mảnh HỖN HỢP (2 điểm)
+
+Câu 6: (1 điểm) Tạo View tên DanhSachTatCaPB_HH:
+
+-- Tạo View DanhSachTatCaPB_HH:
+
+
+-- Test View DanhSachTatCaPB_HH:
+
+
+Câu 7: (1 điểm) Tạo stored procedure tên ThemPB_HH:
+
+-- Tạo PROC ThemPB_HH:
+
+
+-- Test PROC ThemPB_HH:
+
+
+--ĐỂ CÓ ĐIỂM, CÁC EM NHỚ LƯU FILE SQL NÀY VÀO Ổ S:\
+------------------------HẾT-----------------------------------------
+
